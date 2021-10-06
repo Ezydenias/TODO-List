80821252701ebf227e58b2fde95b720d6a2213f0
Ezydenias <kleinermaximilian@googlemail.com>
2021-10-06 10:25:38 +0200

    - added todo chips with a list component containing them

--- src/app/components/todo-chip/todo-chip.component.ts
+++ src/app/components/todo-chip/todo-chip.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ITODO } from 'src/app/interfaces/ITODO';
 
 @Component({
   selector: 'app-todo-chip',
   templateUrl: './todo-chip.component.html',
-  styleUrls: ['./todo-chip.component.scss']
+  styleUrls: ['./todo-chip.component.scss'],
 })
 export class TODOChipComponent implements OnInit {
+  @Input() todo: ITODO = {
+    Id: 0,
+    Label: 'Chip',
+    TODO: '',
+    State: false,
+    DueDate: new Date(),
+  };
+  @Output() crossed = new EventEmitter<number>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  crossOut() {
+    this.todo.State = true;
+    this.crossed.emit(this.todo.Id);
   }
-
 }
--- src/app/components/todo-chips-list/todo-chips-list.component.ts
+++ src/app/components/todo-chips-list/todo-chips-list.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ITODO } from 'src/app/interfaces/ITODO';
 
 @Component({
   selector: 'app-todo-chips-list',
   templateUrl: './todo-chips-list.component.html',
-  styleUrls: ['./todo-chips-list.component.scss']
+  styleUrls: ['./todo-chips-list.component.scss'],
 })
 export class TODOChipsListComponent implements OnInit {
+  @Input() todos: ITODO[] = [
+    { Id: 1, Label: 'eat shit', TODO: '', State: false, DueDate: new Date() },
+    { Id: 2, Label: 'paint humans', TODO: '', State: false, DueDate: new Date() },
+    { Id: 3, Label: 'draw', TODO: '', State: false, DueDate: new Date() },
+    { Id: 4, Label: 'roo', TODO: '', State: false, DueDate: new Date() },
+    { Id: 5, Label: 'eat wallaroo', TODO: '', State: false, DueDate: new Date() },
+  ];
+  @Output() updatedTable = new EventEmitter<ITODO[]>();
+  @Output() OnContextMenu = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  crossedOut(Id:number)
+  {
+    const index = this.todos.findIndex(obj => obj.Id == Id);
+    this.todos[index].State=true;
+    this.updatedTable.emit(this.todos);
   }
 
+  onContextMenu(event: MouseEvent, item: ITODO) {
+
+    // console.log(event.movementX);
+    // console.log(event.screenY);
+    this.OnContextMenu.emit({event,item});
+  }
 }
